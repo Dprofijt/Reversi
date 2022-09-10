c35501745ff65fdfd712e966118a68fae7ef89c8
damian <damian@profijt.eu>
2022-09-10 10:32:13 +0200

    Initial commit

---
+++ gulpfile.js/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    //TODO: path naar server project 
+    localServerProjectPath : '<C:/...pad_naar_server_project_voor_client_bestanden>',
+    files: {
+        js: [
+            'js/**/*.js',
+            'js/*.js'
+        ]
+    },
+    voornaam: 'Damian'
+};
---
+++ gulpfile.js/index.js
@@ -0,0 +1,13 @@
+const config = require('./config');
+
+const js = require('./tasks/js').js(config.localServerProjectPath);
+const js = require('./tasks/js').js(config.voornaam);
+    js.displayName = 'js';
+
+const hello = function (done) {
+    console.log(`Groeten van ${config.voornaam}!`);
+    done();
+};
+
+exports.default = hello;
+exports.js = js;
